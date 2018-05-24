abb24a430bff5dc4beeb1c1d81133b7e8745a5f9
hsc10705581 <934936408@qq.com>
2018-05-24 22:00:40 +0800

    bate2.0

--- sensor_control.ino
+++ sensor_control.ino
@@ -1,10 +1,33 @@
 int sensor1 = 13, sensor2 = 12, sensor3 = 11;
 bool triggerSensor1 = false, triggerSensor2 = false, triggerSensor3 = false;
 bool up = false, low = false, ready_ = true;
+int angle = 90;
+
+#include <Servo.h>
+Servo myservo;
+
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+#include <dht11.h>
+ 
+dht11 DHT11;
+ 
+#define DHT11PIN 2
 
 unsigned long currentMillis, previousMillis = 0;
 
 void setup() {
+  myservo.attach(9);
+  
+  Serial.begin(9600);
+  lcd.begin();
+  lcd.backlight();
+
+  lcd.print("hello world!");
+  
   pinMode(sensor1, INPUT);
   pinMode(sensor2, INPUT);
   pinMode(sensor3, INPUT);
@@ -12,6 +35,10 @@ void setup() {
 }
 
 void loop() {
+  int chk = DHT11.read(DHT11PIN);
+
+  myservo.write(angle);
+  
   // put your main code here, to run repeatedly:
   int x,y,z;
 
@@ -23,7 +50,23 @@ void loop() {
     triggerSensor2 = false;
     triggerSensor3 = false;
     Serial.println("重置，进入待机状态");
+    lcd.clear();
+    lcd.print("Temp: ");
+    lcd.print((float)DHT11.temperature);
+    lcd.setCursor(0, 1);
+    lcd.print("ready!");
+    
     ready_ = true;
+  }
+
+  if (ready_ == true) 
+  {
+    lcd.clear();
+    lcd.print("Temp: ");
+    lcd.print((float)DHT11.temperature);
+    lcd.setCursor(0, 1);
+    lcd.print("ready!");
+    delay(10);
   }
   
   if (triggerSensor1 == false && triggerSensor2 == false && triggerSensor3 == false)
@@ -34,21 +77,18 @@ void loop() {
     if(x == 0)
     {
       triggerSensor1 = true;
-      Serial.println("triggerSensor1 = true;");
       previousMillis = currentMillis;
       ready_ = false;
     }
     if(y == 0)
     {
       triggerSensor2 = true;
-      Serial.println("triggerSensor2 = true;");
       previousMillis = currentMillis;
       ready_ = false;
     }
     if(z == 0) 
     {
       triggerSensor3 = true;
-      Serial.println("triggerSensor3 = true;");
       previousMillis = currentMillis;
       ready_ = false;
     }
@@ -61,7 +101,6 @@ void loop() {
       triggerSensor1 = false;
       triggerSensor2 = true;
       up = true;
-      Serial.println("up = true;");
       previousMillis = currentMillis;
       ready_ = false;
     }
@@ -73,7 +112,6 @@ void loop() {
       triggerSensor3 = false;
       triggerSensor2 = true;
       low = true;
-      Serial.println("low = true");
       previousMillis = currentMillis;
       ready_ = false;
     }
@@ -85,7 +123,6 @@ void loop() {
     {
       triggerSensor2 = false;
       low = true;
-      Serial.println("low = true");
       previousMillis = currentMillis;
       ready_ = false;
     }
@@ -93,7 +130,6 @@ void loop() {
     {
       triggerSensor2 = false;
       up = true;
-      Serial.println("up = true");
       previousMillis = currentMillis;
       ready_ = false;
     }
@@ -101,15 +137,19 @@ void loop() {
   
   if(up == true)
   {
-    Serial.println("temp up");
+    lcd.clear();
+    lcd.print("temp up");
     up = false;
+    if(angle > 40) angle -= 10;
     Serial.println("up = false");
   }
 
   if(low == true)
   {
-    Serial.println("temp low");
+    lcd.clear();
+    lcd.print("temp low");
     low = false;
+    if(angle < 140) angle += 10;
     Serial.println("low = false");
   }
   
